c513755d18317de8ac2a01d353a8c5ac18ca263a
thuvito22 <thuvito22@gmail.com>
2025-02-04 09:48:33 +0100

    Comit Fer repo1

--- src/main/java/es/curso/springboot1/controllers/SocioController.java
+++ src/main/java/es/curso/springboot1/controllers/SocioController.java
@@ -1,13 +1,28 @@
 package es.curso.springboot1.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import es.curso.springboot1.negocio.Socio;
+
 
 @Controller
 public class SocioController {
+
+    List<Socio> socios= new ArrayList<Socio>();
+
+    public SocioController() {
+
+        socios.add(new Socio());
+    }
+    
+
+
     @GetMapping("/socios")
     public String socios() {
         return "plantillasocio";
@@ -21,5 +36,6 @@ public class SocioController {
         return "plantillaversocio";
     }
     
-     
+
+      
 }
